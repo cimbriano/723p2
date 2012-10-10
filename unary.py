1ea77b19f7d3f94fd66e51f4f19087fe7858a664
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-10-10 14:45:45 -0400

    Added part II test in script

--- unary.py
+++ unary.py
@@ -3,6 +3,16 @@ from util import *
 from tree import *
 from grammar import *
 
+print "Part I: Unary Rules"
 print str(timeFliesPCFG)
 print timeFliesSent
 print parse(timeFliesPCFG, timeFliesSent) # produced None , which is wrong
+
+print
+print "Part II: Warming up with Time Flies"
+myTree = parse(timeFliesPCFG2, timeFliesSent)
+print "myTree: "
+print myTree
+print "desiredTimeFliesParse: "
+print desiredTimeFliesParse
+print evaluate(desiredTimeFliesParse, myTree)
